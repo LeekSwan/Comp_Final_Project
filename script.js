d809c9fb74f5f31bab5f9a1b10464ddbb75e05db
kristikaz <kristi.kazmierczak@gmail.com>
2019-11-29 12:19:06 -0500

    adding script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+export const renderSite = function() {
+    const $root = $('#root');
+    renderTweet();
+ 
+    //here is where you will add button handlers, and any extra stuff you need to link to your async helper functions. You can also do this directly in the main function but its not recommended.
+ 
+}
+ 
+$(function () {
+    renderSite(); 
+    }); //this is your main function. I recommend looking at a04 and a07 if you don’t understand it
