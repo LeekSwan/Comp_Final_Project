7b82394fa356a9b8374abec065ce90cb5208e6e6
kristikaz <kristi.kazmierczak@gmail.com>
2019-12-11 21:00:30 -0500

    kk is updating

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var ids = 0; 
 
-
+var idNUM = 0;
 export const renderSite = function() {
     const $root = $('#root');
     //here is where you will add button handlers, and any extra stuff you need to link to your async helper functions. You can also do this directly in the main function but its not recommended.
-
 
     //Home
     $(document).on('click', '#home',  function(e){
@@ -17,57 +15,82 @@ export const renderSite = function() {
         e.preventDefault(); 
         getLeaderBoard(); 
     });
-    
-
 
 
 
     //To-Do
     $(document).on('click', '#toDo', function(e){
-        e.preventDefault(); 
         getToDo(); 
     });
-
-     // list functionality 
-     $(document).on("keyup", ".userInput" ,function(e) {
-        e.preventDefault();         
-        var idNUM = 0;         
-        //13  means enter button
-        if(e.keyCode == 13 && $(".userInput").val() != "")
-        {
-            e.preventDefault();
-            var task = $(`<div id=l${idNUM} class='task'></div>`).text($(".userInput").val());
-            idNUM++; 
-            addTODO(idNUM, $(".userInput").val()); 
-
-
-          //for checkmark
-          var check = $("<i class='fas fa-check' style='float:right; margin-right: 20px;'></i>").click(function(){
-            var p = $(this).parent();
-            //$(".completed").append(p);
-            p.fadeOut(function(){
-              $(".completed").append(p);
-              p.fadeIn();
-            }); 
-            $(this).remove();
-          });
-
-          //for delete
-          var del = $("<i class='fas fa-trash' style='float:right; margin-right: 20px;'></i>").click(function(){
-            var p = $(this).parent();
-            //p.remove();
-            p.fadeOut(function(){
-              p.remove();
-            });
-          });
-
-          task.append(del,check);
-          $(".notCompleted").append(task);
-            //to clear the input
-          $(".userInput").val("");
-          return false;
+        
+    $(document).on('click','#test', function(event){
+        event.preventDefault();
+        console.log('testing');
+        var token = "Bearer " + localStorage.getItem('jwt');
+        const pubRoot = new axios.create({
+            headers: {Authorization: token},
+            baseURL: "http://localhost:3000/user"
+        });
+    
+        async function createTODO(todo, key) {
+          return await pubRoot.post(`/TODO/${idNUM}`, {
+            data: {["task"]:todo, ["key"]: key},
+            type: "merge"
+          })
         }
-      });
+ 
+        (async () => {
+            var task = $(".userInput").val();
+            idNUM++;
+            await createTODO(task, idNUM);
+            
+        })();
+    
+    });
+    
+    
+    
+        
+        // list functionality 
+        $(document).on("keyup", ".userInput" ,async function(e) {
+            e.preventDefault();         
+            //13  means enter button
+            if(e.keyCode == 13 && $(".userInput").val() != "")
+            {
+                e.preventDefault();
+                var task = $(`<div id=l${idNUM} class='task'></div>`).text($(".userInput").val());
+                addTODO(idNUM, $(".userInput").val());  
+                idNUM++; 
+                
+                
+              //for checkmark
+              var check = $("<i class='fas fa-check' style='float:right; margin-right: 20px;'></i>").click(function(){
+                var p = $(this).parent();
+                //$(".completed").append(p);
+                p.fadeOut(function(){
+                  $(".completed").append(p);
+                  p.fadeIn();
+                }); 
+                $(this).remove();
+              });
+    
+              //for delete
+              var del = $("<i class='fas fa-trash' style='float:right; margin-right: 20px;'></i>").click(function(){
+                var p = $(this).parent();
+                //p.remove();
+                p.fadeOut(function(){
+                  p.remove();
+                });
+              });
+    
+              task.append(del,check);
+              $(".notCompleted").append(task);
+                //to clear the input
+              $(".userInput").val("");
+
+            }
+          });
+    
     
     
     //Calendar
@@ -81,6 +104,7 @@ export const renderSite = function() {
     });
 
 
+    
 
 
 
@@ -94,14 +118,8 @@ $(function () {
 }); 
 
 
-export function getUserHomeInfo() {
+ function getUserHomeInfo() {
     const $root = $('#root');
-
-    /*const $root = $('#root');
-    const result = await axios({
-        method: 'get',
-        url: 'http://localhost:3000/public/users'
-    })*/
 
     let screen = `
         <section id="root">
@@ -119,12 +137,14 @@ export function getUserHomeInfo() {
     $root.replaceWith(screen); 
 }
 
-export async function getToDo(){
+ async function getToDo(){
     const $root = $('#root');
     let screen = document.createElement('section');
     screen.innerHTML = `
     <section id="root">
         <div id="list" class="container">
+        <form id = 'formname' onsubmit = "async(e) =>{
+            e.preventDefault();}">
             <div class="notCompleted">
             <h3>Not Completed</h3>
             </div>
@@ -132,7 +152,9 @@ export async function getToDo(){
             <h3>Completed</h3>
             </div>
             <input type="text" class="userInput" placeholder="Put the things you will procrastinate on here">
+            <button type="submit" onSubmit id="test">Add</button>
             </div>
+            </form>
         </div>
     </section>
     `
@@ -140,22 +162,25 @@ export async function getToDo(){
 
 }
 
+
+
 // add to To do
 async function addTODO(idNUM, task){
-    var id = idNUM;
+    console.log("in addTODO");
     axios.post('http://localhost:3000/user/TODO' ,
     {data: {
-      [id] : task
+      [idNUM] : {data: task, ["key"] : idNUM}
+      
     }},
     {headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` }},
     {type: "merge"}
   ).then(function(res){
-    console.log("added todo");
+    console.log("added");
   }).catch(error =>{
     console.log(error);
   });
-
 }
+
 
 // delete to do 
 async function deleteTODO(){
@@ -176,38 +201,101 @@ async function deleteTODO(){
 
 
 
-export async function getLeaderBoard(){
+ async function getLeaderBoard(){
     const $root = $('#root');
-    let screen = document.createElement('section');
-    screen.innerHTML = `
-    <section id="root">
-        <h1>Leader board</h1>
+    var token = "Bearer " + localStorage.getItem('jwt');
+    const pubRoot = new axios.create({
+        headers: {Authorization: token},
+        baseURL: "http://localhost:3000/private"
+    });
 
-    </section>`
+    async function getScores() {
+      return await pubRoot.get(`/scores`);
+    }
 
-    $root.replaceWith(screen);  
+    (async () => {
+       let data = await getScores();
+       let obj = JSON.stringify(data, null, 2); 
+       //["data"]["result"]["r"]["name"]
+       
+       /*for(var i in data.data.result){
+           console.log(data.data.result[i].name);
+       }*/
+       
+       let screen = document.createElement('section');
+       screen = `<section id="root">
+            <h1 class="title">Most Studious of Students</h1>
+            <h2 class="subtitle">How do you compare?</h2>
+            <div class="columns is-multiline is-mobile">
+            <div class="column is-one-quarter">
+                <code>Rank</code>
+            </div>
+            <div class="column">
+                <code>Student</code>
+            </div>
+            <div class="column is-one-quarter">
+                <code>Score</code>
+            </div>
+        </div>
+            <section id="students">
+
+            </section>
+            </section>
+       `;
+       $root.replaceWith(screen); 
+
+       var place = 1; 
+       for(var i in data.data.result){
+            let info = document.createElement('div');
+
+            var name = data.data.result[i].name;
+            var score = data.data.result[i].score;
+            
+
+            info.innerHTML=`
+            <div class="columns is-multiline is-mobile">
+                <div class="column is-one-quarter">
+                    <code>${place}</code>
+                </div>
+                <div class="column">
+                    <code>${name}</code>
+                </div>
+                <div class="column is-one-quarter">
+                    <code>${score}</code>
+                </div>
+            </div>
+                    `;
+            document.getElementById('students').appendChild(info);
+            place++; 
+       }
+    })();
+    
+    
+    
+
+     
 }
 
-export async function getInspired(){
+ async function getInspired(){
     const $root = $('#root');
     let screen = document.createElement('section');
     screen.innerHTML = `
     <section id="root">
         <h1>Inspirational Quote</h1>
 
-    </section>`
+    </section>`;
 
     $root.replaceWith(screen);  
 }
 
-export async function getChat(){
+ async function getChat(){
     const $root = $('#root');
     let screen = document.createElement('section');
     screen.innerHTML = `
     <section id="root">
         <h1>Chat room here</h1>
 
-    </section>`
+    </section>`;
     $root.replaceWith(screen);  
 }
 
@@ -219,17 +307,3 @@ export async function getChat(){
 
 
 
-
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementById("tabcontent");
-    /*for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }*/
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
